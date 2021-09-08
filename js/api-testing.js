26afeb657956d05bd925927898f60e4873ceec09
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-09-08 16:13:58 -0600

    working on more features testing

--- js/api-testing.js
+++ js/api-testing.js
@@ -1,7 +1,7 @@
 (function () {
 	"use strict";
 
-	initializeCalendar();
+	// initializeCalendar();
 
 	function initializeCalendar() {
 
@@ -51,5 +51,67 @@
 
 
 	}
+
+	//***INFINITE SCROLLING****
+	let listContent;
+	let listEnd = document.getElementById("list-end")
+	let itemCount = 0;
+	//This var will be used to check if divItems or ContentItems are already being added to listContent
+	let isAppended = false;
+
+	function infiniteScrolling(){
+		loadInitialContent()
+		addSubDivs()
+		addContentToListContent()
+	}
+
+	window.addEventListener("DOMContentLoaded", loadInitialContent)
+
+	function loadInitialContent(){
+		addSubDivs()
+	}
+
+	function addSubDivs(){
+		listContent = document.getElementById("list-content")
+		// listEnd = document.getElementById("list-end")
+		//When this is called, it will set isAppended to true saying that these next items will be appended
+		isAppended = true;
+		//Will run a loop to addContentToListContent
+		for (let i =0; i < 50; i++ ){
+			let childDiv = addContentToListContent(`This is childdiv #${i+1}`)
+			listContent.appendChild(childDiv)
+			itemCount++
+		}
+		//After items have been appended, we set isAppeneded back to false
+		isAppended = false;
+	}
+
+	function addContentToListContent(message){
+		let childDiv = document.createElement("div");
+		childDiv.setAttribute("class", 'childDiv')
+		childDiv.textContent = message;
+		return childDiv;
+	}
+	let options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 1.0
+	}
+
+	let callback = (entries, observer) =>{
+		entries.forEach(entry => {
+			console.log(entry)
+			if(entry.target.id === 'list-end'){
+				if (entry.isIntersecting){
+				console.log('Call back function called')
+					addSubDivs();
+				} else{
+					console.log('Div cannot be seen')
+				}
+			}
+		})
+	}
+	let observer = new IntersectionObserver(callback, options)
+	observer.observe(listEnd)
 
 })();
